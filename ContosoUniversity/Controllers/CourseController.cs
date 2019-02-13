6d6ad9dec3222772e5c99b8f82be9ba70e186501
delsuckahh <zdelcoco@gmail.com>
2019-02-12 20:40:41 -0600

    Completed Tutorial

--- ContosoUniversity/Controllers/CourseController.cs
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -152,5 +152,20 @@ namespace ContosoUniversity.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewBag.RowsAffected = db.Database.ExecuteSqlCommand("UPDATE Course SET Credits = Credits * {0}", multiplier);
+            }
+            return View();
+        }
     }
 }
